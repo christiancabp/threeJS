461d2c9bd051e902cda45e6f822973562f0eee86
Christian Bermeo <christiancabp@gmail.com>
2022-06-06 01:29:40 -0400

    11-textures

---
+++ 11-textures/src/script.js
@@ -0,0 +1,167 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { TextureLoader } from "three";
+
+/**
+ * Textures
+ */
+
+// First Aproach
+// const image = new Image();
+// const texture1 = new THREE.Texture(image);
+// image.onload = () => {
+//   console.log("image loaded");
+//   texture1.needsUpdate = true;
+// };
+// image.src = "/textures/door/color.jpg";
+
+// console.log(texture1);
+
+//Loading manager for loading events
+
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = () => {
+  //function that runs when it starts loading
+  console.log("onStart");
+};
+loadingManager.onLoad = () => {
+  console.log("onLoded");
+};
+loadingManager.onProgress = () => {
+  console.log("onProgress");
+};
+loadingManager.onError = () => {
+  console.log("Error loading");
+};
+
+// Second Aproach
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load(
+  "/textures/minecraft.png"
+  // // other optional parameters.
+  // () => {
+  //     console.log('load') //function that runs when you load the img.
+  // }
+  // () => {
+  //     console.log('progress')
+  // }
+  // () => {
+  //     console.log('error')
+  // }
+);
+
+//Loading more textures
+
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const ambientOcclusionTexture = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+
+// Modifing texture wrap
+// colorTexture.repeat.x = 4;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.RepeatWrapping; //Also THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping;
+// colorTexture.offset.x = 0.5;
+// colorTexture.offset.y = 0.5;
+
+// colorTexture.rotation = Math.PI / 4;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+colorTexture.minFilter = THREE.NearestFilter;
+colorTexture.generateMipmaps = false; //when using ^ we can turnoff MIPMAPPING and we gain performance
+colorTexture.magFilter = THREE.NearestFilter;
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Object
+ */
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 1;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
